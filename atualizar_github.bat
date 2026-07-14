4002f8adb38e2666550b7cb05e46b2bee0949133
CompartilharNE <contato@compartilharne.com.br>
2026-07-14 15:21:13 -0300

    Atualizacao em 14/07/2026 15:21:13,48

---
+++ atualizar_github.bat
@@ -0,0 +1,27 @@
+@echo off
+cd /d "%~dp0"
+echo ===============================================
+echo   Atualizando Sistema Pontti no GitHub
+echo ===============================================
+echo.
+
+git add .
+
+set /p MSG="Descreva a atualizacao (ou so aperte Enter): "
+if "%MSG%"=="" set MSG=Atualizacao em %date% %time%
+
+git commit -m "%MSG%"
+if errorlevel 1 (
+    echo.
+    echo Nada novo para enviar ou houve um erro no commit acima.
+)
+
+git push
+
+echo.
+echo ===============================================
+echo   Concluido! Confira em:
+echo   https://github.com/CompartilharNE/Sistema-CompartilharNE
+echo ===============================================
+echo.
+pause
